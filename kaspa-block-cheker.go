23a0e9e6e740399d8b7ba571bb87d9d9037e50d8
Tomer Levy <tmrlvi@gmail.com>
2021-12-10 19:30:10 +0200

    Added block color and showing only the child in the selected chain

--- kaspa-block-cheker.go
+++ kaspa-block-cheker.go
@@ -2,83 +2,51 @@ package main
 
 import (
   "encoding/hex"
-  "encoding/json"
-  //"encoding/binary"
   "fmt"
   "os"
   "errors"
 
   "github.com/kaspanet/kaspad/util/bech32"
-  "github.com/kaspanet/kaspad/infrastructure/network/rpcclient/grpcclient"
+  "github.com/kaspanet/kaspad/infrastructure/network/rpcclient"
+  "github.com/kaspanet/kaspad/app/appmessage"
 )
+
 
 const (
   prefix = "kaspa"
 )
-
-type RPCError struct {
-  Message string `json:"message"`
-}
-
-type RPCBlock struct {
-  Transactions []RPCTransaction `json:"transactions"`
-  VerboseData RpcBlockVerboseData `json:"verboseData"`
-}
-
-type RpcBlockVerboseData struct {
-  ChildrenHashes []string `json:"childrenHashes"`
-}
-
-type RPCTransaction struct {
-  Payload string `json:"payload"`
-  Inputs []RPCTransactionInput `json:"inputs"`
-  Outputs []RPCTransactionOutput `json:"outputs"`
-  VerboseData RpcTransactionVerboseData `json:"verboseData"`
-}
-
-type RPCTransactionInput struct {
-}
-
-type RPCTransactionOutput struct {
-  ScriptPublicKey RpcScriptPublicKey `json:"scriptPublicKey"`
-}
-
-type RpcTransactionVerboseData struct {
-  TransactionId string `json:"transactionId"`
-}
-
-type RpcScriptPublicKey struct {
-  ScriptPublicKey string `json:"scriptPublicKey"`
-}
-
-type GetBlockRequestMessage struct {
-  Hash string `json:"hash"`
-  IncludeTransactions bool `json:"includeTransactions"`
-}
-
-type GetBlockResponseMessage struct {
-  Block *RPCBlock `json:"block"`
-  Error *RPCError `json:"error,omitempty"`
-}
-
-type Message struct {
-  GetBlockRequest *GetBlockRequestMessage `json:"getBlockRequest,omitempty"`
-  GetBlockResponse *GetBlockResponseMessage `json:"getBlockResponse,omitempty"`
-}
-
 
 func printErrorAndExit(message string) {
 	fmt.Fprintf(os.Stderr, fmt.Sprintf("%s\n", message))
 	os.Exit(1)
 }
 
-func getCoinBase(transactions []RPCTransaction) (RPCTransaction, error) {
+func colorInfo(block *appmessage.RPCBlock, chain *appmessage.GetVirtualSelectedParentChainFromBlockResponseMessage) (int, string) {
+  isRed := 0
+  // If we found a child, we must be either red or blue
+  for _, hash := range chain.AddedChainBlockHashes {
+    for _, child := range block.VerboseData.ChildrenHashes {
+      if child == hash {
+        isRed = -1
+        for _, redHash := range chain.RemovedChainBlockHashes {
+          if redHash == block.VerboseData.Hash {
+            isRed = 1
+          }
+        }
+        return isRed, child
+      }
+    }
+  }
+  return isRed, ""
+}
+
+func getCoinBase(transactions []*appmessage.RPCTransaction) (*appmessage.RPCTransaction, error) {
   for i := range transactions {
     if len(transactions[i].Inputs) == 0 && len(transactions[i].Payload) > 0{
       return transactions[i], nil
     }
   }
-  return RPCTransaction{}, errors.New("Could not find coinbase")
+  return nil, errors.New("Could not find coinbase")
 }
 
 func payloadToScriptAddressAndVersion(prefix, payloadHex string) ([]byte, byte) {
@@ -105,32 +73,17 @@ func ScriptAddressToAddress(script []byte, version byte) string {
   return bech32.Encode(prefix, address, byte(version))
 }
 
-func getBlockWithTransactions(client *grpcclient.GRPCClient, hash string) *RPCBlock {
-  request := Message{GetBlockRequest: &GetBlockRequestMessage{Hash: hash, IncludeTransactions: true}}
-  requestJson, err := json.Marshal(request)
+func getBlockWithTransactions(client *rpcclient.RPCClient, hash string) *appmessage.RPCBlock {
+  response, err := client.GetBlock(hash, true)
   if err != nil {
-    printErrorAndExit(fmt.Sprintf("error encoding JSON: %s", err))
+    printErrorAndExit(fmt.Sprintf("error getting block: %s", err))
   }
-
-  responseString, err := client.PostJSON(string(requestJson))
-  if err != nil {
-    printErrorAndExit(fmt.Sprintf("error posting the request to the RPC server: %s", err))
-  }
-  if !json.Valid([]byte(responseString)) {
-    printErrorAndExit(fmt.Sprintf("returned json is invalid"))
-  }
-
-  var response Message
-  err = json.Unmarshal([]byte(responseString), &response)
-  if err != nil {
-    printErrorAndExit(fmt.Sprintf("error parsing repsonse: %s", err))
-  }
-  return response.GetBlockResponse.Block
+  return response.Block
 }
 
-func isAddressInTransactions(address []byte, transactions []RPCTransactionOutput) bool {
+func isAddressInTransactions(address []byte, transactions []*appmessage.RPCTransactionOutput) bool {
   for i := range transactions {
-    tran_script, _ := hex.DecodeString(transactions[i].ScriptPublicKey.ScriptPublicKey)
+    tran_script, _ := hex.DecodeString(transactions[i].ScriptPublicKey.Script)
     if string(tran_script) == string(address) {
       return true
     }
@@ -146,7 +99,7 @@ func main() {
   blockHash := os.Args[1]
   //prefix := os.Args[2] // kaspa
 
-  client, err := grpcclient.Connect("localhost:16110")
+  client, err := rpcclient.NewRPCClient("localhost:16110")
   if err != nil {
     printErrorAndExit(fmt.Sprintf("error connecting to the RPC server: %s", err))
   }
@@ -161,20 +114,47 @@ func main() {
   if err != nil {
     printErrorAndExit(fmt.Sprintf("error while search for coinbase: %s", err))
   }
+
   script, version := payloadToScriptAddressAndVersion(prefix, coinbaseTran.Payload)
-  fmt.Println(fmt.Sprintf("Block %s.", blockHash))
+  fmt.Println(fmt.Sprintf("Block %s", blockHash))
   fmt.Println(fmt.Sprintf("Mined by: %s (accroding to the payload).", ScriptAddressToAddress(script, version)))
-  if len(block.VerboseData.ChildrenHashes) == 0 {
+
+  virtualChain, err := client.GetVirtualSelectedParentChainFromBlock(block.VerboseData.Hash)
+  if err != nil {
+    printErrorAndExit(fmt.Sprintf("error could not get virtual chain: %s", err))
+  }
+  isRed, selectedChild := colorInfo(block, virtualChain)
+
+  if isRed == 1{
+    fmt.Println("\u274c Block color is \033[31mred\033[0m.")
+  } else if isRed == -1 {
+    fmt.Println("\u2705 Block color is \033[34mblue\033[0m.")
+  } else {
+    fmt.Println("\u2754 Block color is not determined yet.")
+  }
+
+
+  if (selectedChild != ""){
+    child_block := getBlockWithTransactions(client, selectedChild)
+    child_coinbase, _ := getCoinBase(child_block.Transactions)
+    if isAddressInTransactions(script, child_coinbase.Outputs) {
+      fmt.Println(fmt.Sprintf("\u2705 Child's coinbase transaction %s contains the address.", child_coinbase.VerboseData.TransactionID))
+    } else {
+      fmt.Println(fmt.Sprintf("\u274c Child's coinbase transaction %s does not contain the address.", child_coinbase.VerboseData.TransactionID))
+    }
+  }
+
+  /*if len(block.VerboseData.ChildrenHashes) == 0 {
     fmt.Println("The block does not have childern. Perhaps was not validated yet.")
   } else {
     for i := range block.VerboseData.ChildrenHashes {
       child_block := getBlockWithTransactions(client, block.VerboseData.ChildrenHashes[i])
       child_coinbase, _ := getCoinBase(child_block.Transactions)
       if isAddressInTransactions(script, child_coinbase.Outputs) {
-        fmt.Println(fmt.Sprintf("\u2714 Child's coinbase transaction %s contains the address.", child_coinbase.VerboseData.TransactionId))
+        fmt.Println(fmt.Sprintf("\u2705 Child's coinbase transaction %s contains the address.", child_coinbase.VerboseData.TransactionID))
       } else {
-        fmt.Println(fmt.Sprintf("\u274c Child's coinbase transaction %s does not contain the address.", child_coinbase.VerboseData.TransactionId))
+        fmt.Println(fmt.Sprintf("\u274c Child's coinbase transaction %s does not contain the address.", child_coinbase.VerboseData.TransactionID))
       }
     }
-  }
+  }*/
 }
